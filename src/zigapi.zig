3b8403ea44225b4ede756d58f00af69e5286e27f
julek <dnjulek@hotmail.com>
2025-04-15 16:25:33 -0300

    Clean unused

--- src/zigapi.zig
+++
@@ -1,5 +0,0 @@
-pub const ZFrameRO = @import("zigapi/ZFrameRO.zig");
-pub const ZFrameRW = @import("zigapi/ZFrameRW.zig");
-pub const ZMapRO = @import("zigapi/ZMapRO.zig");
-pub const ZMapRW = @import("zigapi/ZMapRW.zig");
-pub const ZAPI = @import("zigapi/ZAPI.zig");
