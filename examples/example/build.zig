37dc51496d2c1236b8dd100d17f9db53334790bc
dnjulek <dnjulek@hotmail.com>
2025-08-03 13:56:52 -0300

    vsapi 4.0 example

--- examples/example/build.zig
+++ examples/example/build.zig
@@ -17,6 +17,7 @@ pub fn build(b: *std.Build) void {
     const vapoursynth_dep = b.dependency("vapoursynth", .{
         .target = target,
         .optimize = optimize,
+      //.vsapi4_minor = .minor_0, // if you want to use outdated vapoursynth
     });
 
     lib.root_module.addImport("vapoursynth", vapoursynth_dep.module("vapoursynth"));
